d6efb068bd7ab22a7356221d8b713569a6cb2f8d
Odiepriye1 <97673996+Odiepriye1@users.noreply.github.com>
2024-02-23 09:18:15 +0000

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.InsertOneResult;
+import org.bson.BsonValue;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import java.util.Arrays;
+
+public class Main {
+    MongoCollection<Document> collection;
+    public void dbConnection(){
+        String connectionString = "mongodb+srv://ody:Ody12345@cluster0.guk08bz.mongodb.net/?retryWrites=true&w=majority";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            MongoDatabase database = mongoClient.getDatabase("blog");
+            collection = database.getCollection("posts");
+//            dbInsert();
+//            createIndexes();
+//            dbFind();
+//            dbUpdate();
+//            dbDelete();
+//            dbAggregate();
+        }
+    }
+    private void dbInsert(){
+        Document document = new Document("name", "soduari")
+            .append("age", 25)
+            .append("city", "New York");
+        collection.insertOne(document);
+        System.out.println("Document inserted successfully.");
+    }
+    private void createIndexes(){
+        collection.createIndex(new Document("name", 1));
+        System.out.println("Index created successfully.");
+    }
+    private void dbFind() {
+        Document foundDocument = collection.find(Filters.eq("name", "soduari")).first();
+        System.out.println("Found document: " + foundDocument.toJson());
+    }
+    private void dbUpdate() {
+        collection.updateOne(Filters.eq("name", "soduari"), new Document("$set", new Document("city", "Los Angeles")));
+        System.out.println("Document updated successfully.");
+    }
+    private void dbDelete() {
+        collection.deleteOne(Filters.eq("name", "soduari"));
+        System.out.println("Document deleted successfully.");
+    }
+    private void dbAggregate() {
+        Bson matchStage = Aggregates.match(Filters.eq("name:","soduari"));
+        collection.aggregate(Arrays.asList(matchStage)).forEach(document -> System.out.println(document.toJson()));
+        System.out.println("Aggregation result:");
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.dbConnection();
+    }
+}
+
